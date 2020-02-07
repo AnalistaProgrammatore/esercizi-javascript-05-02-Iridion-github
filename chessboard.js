26a40e325785e9ad93efdb6904ba17e11d52f59e
iridion <antinoridavide@outlook.it>
2020-02-07 15:29:45 +0100

    Svista su chessboard fixata.

--- chessboard.js
+++ chessboard.js
@@ -3,14 +3,44 @@
 * separate lines.
 * At each position of the grid there is either a space or a "#" character.
 * The characters should form a chessboard.
-*/
 
+When you have a program that generates this pattern, define a binding size = 8 and change the program 
+so that it works for any size, outputting a grid of the given width and height.
+*/
+/*
 let chessboard = ''
-for (let x = 0; x < 8; x++) {
+let size = 8 
+for (let x = 0; x < size; x++) {
   if (x % 2 === 0) {
     chessboard += '# # # # \n'
   } else {
     chessboard += ' # # # #\n'
+  }
+}
+
+console.log(chessboard)
+*/
+
+
+let chessboard = ''
+let size = 8
+for (let x = 0; x < size; x++) {
+  for (let y = 0; y < size; y++) {
+    if (y === 7) {
+      chessboard += '\n'
+    } else if (x % 2 === 0) {
+      if (y % 2 === 0) {
+        chessboard += '#'
+      } else {
+        chessboard += ' '
+      }
+    } else {
+      if (y % 2 !== 0) {
+        chessboard += '#'
+      } else {
+        chessboard += ' '
+      }
+    }
   }
 }
 
